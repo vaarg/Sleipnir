de8c42286192f4d0b07ea38ba2587b8d16be61c8
vaarg <104344158+vaarg@users.noreply.github.com>
2022-06-08 20:24:36 +1000

    Update Sleipnir.py

--- Sleipnir.py
+++ Sleipnir.py
@@ -1,12 +1,44 @@
 #!/bin/python
 
-# Sleipnir v 0.1
+# Sleipnir v 0.3
+
+from Cryptodome.Cipher import AES
+from Cryptodome.Util.Padding import pad, unpad
 
 import argparse
+import base64
 import socket
 import threading
 
-# Server:
+# [=] Common:
+
+def passwordSet(passwordSess):
+    global password
+    password = passwordSess
+
+    if len(password) < 16:
+        i = len(password) # 15
+        while i < 16:
+            password += b'A'
+            i += 1
+    elif len(password) > 16:
+        password = password[0:16]
+
+# [#] Encryption/Decryption:
+
+def encryptMsg(data):
+    global ciphered_data
+    cipher = AES.new(password, AES.MODE_CBC, iv)
+    ciphered_data = cipher.encrypt(pad(data, AES.block_size))
+    ciphered_data = base64.encodebytes(ciphered_data)
+
+def decryptMsg(ciphered_data):
+    global decrypted_data
+    ciphered_data = base64.decodebytes(ciphered_data)
+    cipher = AES.new(password, AES.MODE_CBC, iv)
+    decrypted_data = unpad(cipher.decrypt(ciphered_data), AES.block_size)
+
+# [<] Server:
 
 def connector():
     while True:
@@ -14,24 +46,41 @@ def connector():
         print(f"{clientAddress} has connected.")
         clientSocket.send(bytes("Enter in username and then press [ENTER]: ", encoding))
         addresses[clientSocket] = clientAddress
-        threading.Thread(target=handler, args=(clientSocket,), daemon=True).start()
+        threading.Thread(target=handler, args=(clientSocket,clientAddress), daemon=True).start()
 
-def handler(clientSocket):
+def handler(clientSocket, clientAddress):
     username = clientSocket.recv(byteSize).decode(encoding)
     success = f"You have connected as {username}, type '{terminatingStr}' to terminate connection at anypoint."
     clientSocket.send(bytes(success, encoding))
     clients[clientSocket] = username
-    message = f"{username} has connected!"
-    broadcaster(bytes(message, encoding))
+    clientSocket.send(bytes("\nEnter session password and the press [ENTER]: ", encoding))
+    cliPass = clientSocket.recv(byteSize)    
     sending = threading.Thread(target=serverSend, daemon=True)
     sending.start()
+    mode = 0
 
     while True:
+        if mode == 0:
+            if cliPass == password:
+                mode = 1 
+                pass       
+            else:
+                kill = f"{terminatingStr}"
+                clientSocket.send(bytes(kill, encoding))
+                clientSocket.close()
+                del clients[clientSocket]
+                broadcaster(bytes(f"{clientAddress} has been disconnected: Incorrect session password.", encoding))
+                break
+        if mode == 1:
+            mode = 2
+            message = f"{username} has connected!"
+            broadcaster(bytes(message, encoding))
+            pass
+        if mode == 2:
             message = clientSocket.recv(byteSize)
             if message != bytes("q!", encoding):
                 broadcaster(message, username+": ")       
             else:
-                clientSocket.send(bytes("q!",encoding))
                 clientSocket.close()
                 del clients[clientSocket]
                 broadcaster(bytes(f"{username} has disconnected.", encoding))
@@ -61,12 +110,26 @@ def serverInit():
     newThread.join() 
     server.close()
 
-# Client:
+# [>] Client:
 
 def receiver():
     while True:
         messageRecv = clientSock.recv(byteSize).decode(encoding)
-        print(messageRecv)
+        if messageRecv == f"{terminatingStr}":
+            break
+        else:
+            print(messageRecv)  
+    print(f"Incorrect session password entered! Type '{terminatingStr}' to terminate program.")
+
+def sender():
+    while True:
+        messageSend = input()
+        if messageSend == terminatingStr:
+            clientSock.send(bytes(terminatingStr, encoding))
+            break
+        clientSock.send(bytes(messageSend, encoding))
+    print(f"Termination sequence '{terminatingStr}' entered! Terminating client.")
+    clientSock.close()
 
 def clientInit():
     global clientSock
@@ -77,15 +140,7 @@ def clientInit():
     clientThread = threading.Thread(target=receiver, daemon=True)
     clientThread.daemon = True
     clientThread.start()
-
-    while True:
-        messageSend = input()
-        if messageSend == terminatingStr:
-            clientSock.send(bytes(terminatingStr, encoding))
-            break
-        clientSock.send(bytes(messageSend, encoding))
-    print(f"Terminating sequence '{terminatingStr}' entered! Terminating client.")
-    clientSock.close()
+    sender()
 
 # [!] Main
 
@@ -101,8 +156,12 @@ PORT = args.port
 byteSize = 2048
 encoding = "latin-1"
 terminatingStr = "q!"
+iv = 16 * b'\0'
 
 if args.server:
+    passwordSess = bytes(input("Enter server session password: "), 'utf-8')
+    passwordSet(passwordSess)
     serverInit()
 else:
     clientInit()
+
